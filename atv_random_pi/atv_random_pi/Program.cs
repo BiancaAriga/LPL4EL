907b231f048307c1c15a29918140e5919a843e8d
Bianca Ariga <biancaariga45@gmail.com>
2021-05-24 17:00:10 -0300

    atividade de estimativa do valor de pi baseado em um número aleatório

---
+++ atv_random_pi/atv_random_pi/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace random_pi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal pi = 3.1415926535897932384626433m;
+            Console.WriteLine("Valor de pi: " + pi);
+            Random randomName = new Random();
+            int random = randomName.Next(1, 25);
+            Console.WriteLine("Número aleatório para as casas decimais: " + random);
+            decimal pi_random = Math.Round(pi, random);
+            Console.WriteLine("Estimativa do pi: " + pi_random);
+        }
+    }
+}
