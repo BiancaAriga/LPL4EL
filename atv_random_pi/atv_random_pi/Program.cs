008c66865fd6a9f1494953f3e1146e83ed98f40d
Bianca Ariga <biancaariga45@gmail.com>
2021-05-24 21:13:21 -0300

    arrumada atividade de estimativa do pi

--- atv_random_pi/atv_random_pi/Program.cs
+++ atv_random_pi/atv_random_pi/Program.cs
@@ -4,15 +4,26 @@ namespace random_pi
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            decimal pi = 3.1415926535897932384626433m;
-            Console.WriteLine("Valor de pi: " + pi);
-            Random randomName = new Random();
-            int random = randomName.Next(1, 25);
-            Console.WriteLine("Número aleatório para as casas decimais: " + random);
-            decimal pi_random = Math.Round(pi, random);
-            Console.WriteLine("Estimativa do pi: " + pi_random);
+            Random random = new Random();
+            int acertos = 0;
+            int n = 5000000;
+
+            for (int i = 0; i < n; i++)
+            {
+                double x = random.NextDouble();
+                double y = random.NextDouble();
+
+                if (x * x + y * y < 1)
+                {
+                    acertos++;
+                }
+            }
+
+            double pi = (4.0 * acertos / n);
+            Console.WriteLine("O valor de pi é: " + pi);
+
         }
     }
 }
